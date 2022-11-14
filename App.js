0fdcd6eb337ab36fff84757f1875dea8dd154ee5
Kemi Adeyemi <kembalos@hotmail.com>
2022-11-14 18:59:26 +0200

    currency

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { API_KEY } from '@env';
+import { StyleSheet, Text, View, Alert, Button, Image, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [rates, setRates] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('');
+  const [amount, setAmount] = useState('');
+  const [eur, setEur] = useState('');
+
+  const getData = async () => {
+    const url = 'http://api.apilayer.com/exchangerates_data/latest';
+    const headers = new Headers();
+    headers.append('apikey', API_KEY);
+
+    try {
+      const response = await fetch(url, { headers });
+      const currencyData = await response.json();
+      setRates(currencyData.rates);
+    } catch (e) {
+      Alert.alert('Error fetching data');
+    }
+  }
+
+  useEffect(() => { getData() }, []);
+
+  function convert() {
+    const amountEur = Number(amount) / rates[selectedCurrency];
+    setEur(`${amountEur.toFixed(2)}e`);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Text>{eur}</Text>
+      <View>
+        <TextInput
+          style={styles.text}
+          placeholder={'Amount'}
+          keyboardType='numeric'
+          value={amount}
+          onChangeText={text => setAmount(text)}
+        />
+        <Picker style={{ height: 50, width: 100 }}
+          selectedValue={selectedCurrency}
+          onValueChange={(itemValue, itemIndex) => {
+            setSelectedCurrency(itemValue);
+          }}
+        >
+          {Object.keys(rates).sort().map(key => (<Picker.Item label={key} value={key} key={key} />))}
+        </Picker>
+      </View>
+      <Button
+        title='Convert'
+        onPress={convert}
+      />
     </View>
   );
 }
@@ -17,4 +67,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: 'blue'
+  }
 });
